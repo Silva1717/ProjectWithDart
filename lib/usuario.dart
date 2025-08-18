c19943228a424b4244930f22ece4c122792f0b44
Silva1717 <Silva.ro1717@gmail.com>
2025-08-18 11:52:15 -0300

    implementando o encapsulanto na classe usuario

--- lib/usuario.dart
+++ lib/usuario.dart
@@ -1,13 +1,35 @@
-class Usuario{
-  int? codigo;
-  String? nome;
-  String? login;
-  String? senha;
+import 'dart:math';
 
-  Usuario({
-    this.codigo,
-    this.senha,
-    this.login,
-    this.nome
-});
+class Usuario{
+  int? _codigo;
+  String? _nome;
+  String? _login;
+  String? _senha;
+
+  Usuario({int? codigo, String? senha,String? login? nome}){
+    _codigo = codigo;
+    _login = login;
+    _nome = nome;
+    _senha = senha;
+}
+//getters
+//exemplos tradicional
+String? get login{
+    return _login;
+}
+//metodo get ao estilo dart
+int? get codigo => _codigo;
+  String? get nome=> _nome;
+  String? get senha  => _senha;
+
+
+  //setters
+//setter tradicional
+set login(String? login){
+  _login = login;
+}
+//metodo set ao estilo dart
+set nome(String? nome) => _nome = nome;
+set codigo(int? codigo) => _codigo = codigo;
+set senha(String? senha) => _senha = senha;
 }
