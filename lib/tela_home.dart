f17d303b82ecb395683bae42798f5eb8d0e4987c
Silva1717 <silva.ro1717@gmail.com>
2025-09-29 11:42:44 -0300

    Criando tela de cadastro de restaurante

--- lib/tela_home.dart
+++ lib/tela_home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:voupedir/tela_cad_restaurante.dart';
 
 
 class TelaHome extends StatelessWidget{
@@ -9,8 +10,31 @@ class TelaHome extends StatelessWidget{
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Seja bem vindo Usuário ")),
-      body: const Center(
-        child: Text("Tela de Home"),
+      body: Padding(padding: const EdgeInsets.all(30),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("lista de Restaurantes:"),
+            ElevatedButton(onPressed: (){
+              Navigator.push(context, MaterialPageRoute(builder: (context) => TelaCadRestaurante()));
+            }, child: Text("Cadastrar Novo Restaurante")),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Column(
+                  children: [
+                    Text("Meu Restaurante"),
+                    Text("Comida Mexcana")
+                  ],
+                ),
+               SizedBox(width: 10),
+                ElevatedButton(onPressed: (){}, child: Text("Excluir")),
+               SizedBox(width: 10),
+                ElevatedButton(onPressed: (){}, child:  Text("Editar"))
+              ],
+            )
+          ],
+        )
     ),
     );
     throw UnimplementedError();
